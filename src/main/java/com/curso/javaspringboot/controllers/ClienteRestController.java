600f714410191865ea001801de163d6266d4c356
amt <alberto_facha@gmail.com>
2024-03-19 20:47:28 +0100

    cambios

--- src/main/java/com/curso/javaspringboot/controllers/ClienteRestController.java
+++ src/main/java/com/curso/javaspringboot/controllers/ClienteRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.javaspringboot.entitys.Cliente;
 import com.curso.javaspringboot.services.IClienteService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,23 +44,28 @@ public class ClienteRestController {
 
 	@PostMapping("/clientes")
 	@ResponseStatus(HttpStatus.CREATED) // 201 create
-	public Cliente create(@RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> create(@Valid @RequestBody Cliente cliente) {
 
-		return clienteService.save(cliente);
+		return  ResponseEntity.ok().body(clienteService.save(cliente));
 
 	}
 
 	@PutMapping("/clientes/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente update(@RequestBody Cliente cliente,@PathVariable Long id) {
+	public ResponseEntity<Cliente> update(@Valid @RequestBody Cliente cliente,@PathVariable Long id) {
 		Cliente clienteupdate= clienteService.findById(id);
 		
+		if (clienteupdate == null) {
+			// Manejo cuando el cliente no se encuentra, podría ser un mensaje personalizado o simplemente not found
+			return ResponseEntity.notFound().build();
+		}
+
 		clienteupdate.setApellido(cliente.getApellido());
 		clienteupdate.setEmail(cliente.getEmail());
 		clienteupdate.setNombre(cliente.getNombre());
 
 
-		return clienteService.save(clienteupdate);
+		return ResponseEntity.ok().body(clienteService.save(clienteupdate));
 
 
 
--- src/main/java/com/curso/javaspringboot/entitys/Cliente.java
+++ src/main/java/com/curso/javaspringboot/entitys/Cliente.java
@@ -1,16 +1,23 @@
 package com.curso.javaspringboot.entitys;
 
 import java.io.Serializable;
-import java.util.Date;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+
+
+import javax.validation.Valid;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -18,8 +25,8 @@ import lombok.Setter;
 @Setter
 @Getter
 @Entity
-@Table(name="Clientes")
-public class Cliente  implements Serializable{
+@Table(name = "Clientes")
+public class Cliente implements Serializable {
     @NonNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,17 +34,20 @@ public class Cliente  implements Serializable{
     @NonNull
     private String nombre;
     @NonNull
+   
     private String apellido;
     @NonNull
+    @Email(message = "Formato de correo electrónico inválido.")
+    @NotBlank(message = "El correo electrónico no puede estar vacío") 
     private String email;
     @NonNull
-    @Temporal(TemporalType.DATE)
-    private Date fecha;
-    
-    //se crea la fecha de forma automatica
+    // @Temporal(TemporalType.DATE)
+
+    private ZonedDateTime fecha;
+
     @PrePersist
-    public void prePersist(){
-        fecha = new Date();
+    public void prePersist() {
+        fecha = ZonedDateTime.now(ZoneId.of("Europe/Madrid"));
     }
 
 }
